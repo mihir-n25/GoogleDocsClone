39cf58ce6af0d6b8bf4a86da0d284e6b66d9278e
mihir-n25 <mihirnebani@gmail.com>
2024-02-18 11:35:25 +0530

    Styles Added

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -2,16 +2,29 @@ import React, { useCallback } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 
+
 function TextEditor() {
+    const TOOLBAR_OPTIONS = [
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        [{ font: [] }],
+        [{ list: "ordered" }, { list: "bullet" }],
+        ["bold", "italic", "underline"],
+        [{ color: [] }, { background: [] }],
+        [{ script: "sub" }, { script: "super" }],
+        [{ align: [] }],
+        ["image", "blockquote", "code-block"],
+        ["clean"],
+      ]
+
     const wrapperRef = useCallback((wrapper) => {
         if(wrapper == null) return
 
         wrapper.innerHTML = ''
         const editor = document.createElement("div")
         wrapper.append(editor)
-    new Quill(editor, { theme: "snow" });
+    new Quill(editor, { theme: "snow" , modules : {toolbar : TOOLBAR_OPTIONS}});
 } , [])
-return <div id="container" ref={wrapperRef}></div>
+return <div className="container" ref={wrapperRef}></div>
 
 }
 
